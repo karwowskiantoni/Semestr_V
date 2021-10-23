cb327a4d214e4a7028ad8b199a7e21b3ab28c98a
Serionus <68148246+Serionus@users.noreply.github.com>
2021-10-23 16:26:02 +0200

    stable beginning

--- aplikacje_w_jezykach_interpretowanych/zadanie_3/cinemax/src/App.vue
+++ aplikacje_w_jezykach_interpretowanych/zadanie_3/cinemax/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Search />
+    <FilmTable />
+    <GenreList />
+    <CastList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from "./components/Search.vue";
+import FilmTable from "./components/FilmTable.vue";
+import GenreList from "./components/FilmsListByGenre.vue";
+import CastList from "./components/FilmsListByCast.vue";
+import "bootstrap";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Search, FilmTable, GenreList, CastList },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-left: 21vw;
+  margin-right: 21vw;
 }
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ aplikacje_w_jezykach_interpretowanych/zadanie_3/cinemax/src/components/FilmTable.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <table class="table-condensed table-hover">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Production Year</th>
+          <th>Cast</th>
+          <th>Genres</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="movie in movies" :key="movie.title">
+          <td>{{ movie.title }}</td>
+          <td>{{ movie.year }}</td>
+          <td>{{ movie.cast }}</td>
+          <td>{{ movie.genres }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      movies: [
+        {
+          title: "Chained for Life",
+          year: 1951,
+          cast: ["Hilton Twins"],
+          genres: [],
+        },
+        {
+          title: "Cheese Chasers",
+          year: 1951,
+          cast: ["Looney Tunes"],
+          genres: ["Animated"],
+        },
+        {
+          title: "Chicago Calling",
+          year: 1951,
+          cast: ["Dan Duryea", "Mary Anderson"],
+          genres: ["Noir"],
+        },
+        {
+          title: "China Corsair",
+          year: 1951,
+          cast: ["Jon Hall", "Ernest Borgnine"],
+          genres: ["Adventure"],
+        },
+        {
+          title: "So This Is Paris",
+          year: 1955,
+          cast: ["Tony Curtis", "Gloria DeHaven"],
+          genres: ["Musical"],
+        },
+        {
+          title: "Soldier of Fortune",
+          year: 1955,
+          cast: ["Clark Gable", "Susan Hayward"],
+          genres: ["Drama"],
+        },
+        {
+          title: "Son of Sinbad",
+          year: 1955,
+          cast: ["Dale Robertson", "Sally Forrest", "Vincent Price"],
+          genres: ["Adventure"],
+        },
+        {
+          title: "Southbound Duckling",
+          year: 1955,
+          cast: ["Tom and Jerry"],
+          genres: ["Animated"],
+        },
+        {
+          title: "Special Delivery",
+          year: 1955,
+          cast: ["Joseph Cotten", "Eva Bartok"],
+          genres: ["Comedy"],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
