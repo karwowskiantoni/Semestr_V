ebc3795cea18a6368060164e621e15120de997ed
Serionus <68148246+Serionus@users.noreply.github.com>
2021-11-22 23:23:04 +0100

    ruletka i krzyzowanie

--- metaheurystyki/zadanie_2/main.py
+++ metaheurystyki/zadanie_2/main.py
@@ -1,9 +1,26 @@
-from genetic_algorighm import genetic_algorithm, calculate_adaptation, random_individual
+import random
+
+from bitarray.util import urandom
+
+from genetic_algorighm import genetic_algorithm, calculate_adaptation, random_individual, cross_genes, make_pairs
 
 # main function
 # todo
 if __name__ == '__main__':
-    print(calculate_adaptation(random_individual()))
+    # genetic_algorithm(50, 10)
+    # first = urandom(26)
+    # second = urandom(26)
+    # pair = [first, second]
+    # print(first)
+    # print(second)
+    # pivot = random.randint(1, 25)
+    # print(pivot)
+    # cross_genes(pair, pivot)
+
+    debug = [1,4,5,6,7]
+    print(make_pairs(debug))
+
+
 
 
 
