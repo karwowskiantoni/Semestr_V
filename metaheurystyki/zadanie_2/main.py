f873b90c551b9888b017427c3bab2bd2dbb46949
antonikarwowski <229908@edu.p.lodz.pl>
2021-11-24 19:21:52 +0100

    genetic algorithm v3

--- metaheurystyki/zadanie_2/main.py
+++ metaheurystyki/zadanie_2/main.py
@@ -1,26 +1,26 @@
-import random
+from genetic_algorighm import *
+import matplotlib.pyplot as plt
 
-from bitarray.util import urandom
-
-from genetic_algorighm import genetic_algorithm, calculate_adaptation, random_individual, cross_genes, make_pairs
-
-# main function
-# todo
 if __name__ == '__main__':
-    # genetic_algorithm(50, 10)
-    # first = urandom(26)
-    # second = urandom(26)
-    # pair = [first, second]
-    # print(first)
-    # print(second)
-    # pivot = random.randint(1, 25)
-    # print(pivot)
-    # cross_genes(pair, pivot)
 
-    debug = [1,4,5,6,7]
-    print(make_pairs(debug))
+    NUMBERS = 100
+    averages = []
+    for i in range(NUMBERS):
+        sums = []
+        for j in range(20):
+            individuals = genetic_algorithm(50, i+1, 0.9, 0.1, False)
+            sums.append(calculate_population_adaptation(individuals))
+        averages.append(sum(sums) / len(sums))
+        print(i)
 
-
-
-
-
+    averages_2 = []
+    for i in range(NUMBERS):
+        sums = []
+        for j in range(20):
+            individuals = genetic_algorithm(50, i+1, 0.9, 0.1, True)
+            sums.append(calculate_population_adaptation(individuals))
+        averages_2.append(sum(sums) / len(sums))
+        print(i)
+    plt.plot([x for x in range(NUMBERS)], averages)
+    plt.plot([x for x in range(NUMBERS)], averages_2)
+    plt.show()
