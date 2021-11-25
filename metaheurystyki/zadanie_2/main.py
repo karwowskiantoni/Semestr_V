6f91d4cd861b5246d20208a28449eef73a178077
antonikarwowski <229908@edu.p.lodz.pl>
2021-11-25 18:10:05 +0100

    metaheurystyki zad 2 ready

--- metaheurystyki/zadanie_2/main.py
+++ metaheurystyki/zadanie_2/main.py
@@ -4,36 +4,55 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    coefficients = []
-    for item in data.DATA:
-        coefficients.append(float(item[2]/item[1]))
-    best_value = max(coefficients)*data.BAG_MAX_WEIGHT
-
-    NUMBERS = 50
-    x_values = [x+2 for x in range(NUMBERS)]
+    NUMBERS = 100
+    x_values = [x*135000 for x in range(NUMBERS)]
     averages = []
+    averages_2 = []
+    averages_3 = []
+    averages_4 = []
+
     for i in range(NUMBERS):
         sums = []
-        for j in range(20):
-            individuals = genetic_algorithm(is_roulette=False, mutating_probability=i*0.02, is_single_pivot=False)
+        for j in range(50):
+            individuals = genetic_algorithm(is_roulette=False, is_single_pivot=False, wanted_adaptation=i * 135000)
             sums.append(calculate_population_adaptation_avg(individuals))
         averages.append(sum(sums) / len(sums))
         print(i)
 
-    averages_2 = []
     for i in range(NUMBERS):
         sums = []
-        for j in range(20):
-            individuals = genetic_algorithm(is_roulette=False, mutating_probability=i*0.02, is_single_pivot=True)
+        for j in range(50):
+            individuals = genetic_algorithm(is_roulette=True, is_single_pivot=False, wanted_adaptation=i * 135000)
             sums.append(calculate_population_adaptation_avg(individuals))
         averages_2.append(sum(sums) / len(sums))
         print(i)
 
-    print(max(averages))
-    print(max(averages_2))
+    for i in range(NUMBERS):
+        sums = []
+        for j in range(50):
+            individuals = genetic_algorithm(is_roulette=False, is_single_pivot=True, wanted_adaptation=i * 135000)
+            sums.append(calculate_population_adaptation_avg(individuals))
+        averages_3.append(sum(sums) / len(sums))
+        print(i)
+
+    for i in range(NUMBERS):
+        sums = []
+        for j in range(50):
+            individuals = genetic_algorithm(is_roulette=True, is_single_pivot=True, wanted_adaptation=i * 135000)
+            sums.append(calculate_population_adaptation_avg(individuals))
+        averages_4.append(sum(sums) / len(sums))
+        print(i)
+
     plt.plot(x_values, averages)
     plt.plot(x_values, averages_2)
-    plt.plot(x_values, [best_value for x in range(NUMBERS)])
+    plt.plot(x_values, averages_3)
+    plt.plot(x_values, averages_4)
+    plt.plot(x_values, [BAG_MAX_VALUE for x in range(NUMBERS)])
+    plt.legend(["elite selection, two pivots",
+                "roulette selection, two pivots",
+                "elite selection, single pivot",
+                "roulette selection, single pivot",
+                "max found single individual adaptation"])
+    plt.xlabel("expected adaptation")
+    plt.ylabel("average individual adaptation in final population")
     plt.show()
-
-
