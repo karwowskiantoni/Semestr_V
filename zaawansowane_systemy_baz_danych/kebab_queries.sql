6eb6d81ffb2fe3df80657a8f7c70d59ab419ece9
karwowskiantoni <70971641+karwowskiantoni@users.noreply.github.com>
2021-12-06 19:32:03 +0100

    Add files via upload
    mój kurwa pierwszy w życiu commit przez Guja githubowego, brzydzę się sobą teraz

---
+++ zaawansowane_systemy_baz_danych/kebab_queries.sql
@@ -0,0 +1,43 @@
+--1 total_profit per employee
+
+
+--2 total number and price of sold products in each product_type
+
+
+--3 all orders not finished yet
+
+
+--4 average realization time per employee
+
+
+--5 average realization time per product_type
+
+
+--6 all employees with orders actually assigned to them
+
+
+--7 number of orders finished per employee
+
+
+--8 most times finished product_type per employee
+
+
+--9 product_types with employees which never made this product_type
+
+
+--10 total profit generated this month
+
+
+--11 weight / price proportion per each product_type
+
+
+--12 employees which made only vegan products
+
+
+--13 number of product_type changes per specific name (group by name)
+
+
+--14 calculate price of specific order
+
+
+--15 calculate price of specific location
