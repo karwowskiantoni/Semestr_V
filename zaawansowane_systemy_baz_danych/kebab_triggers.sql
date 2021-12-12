e3b9c926bd9e953eb63e3cefca4b3b6b3472f545
Serionus <68148246+Serionus@users.noreply.github.com>
2021-12-12 22:57:17 +0100

    bazy v1

--- zaawansowane_systemy_baz_danych/kebab_triggers.sql
+++ zaawansowane_systemy_baz_danych/kebab_triggers.sql
@@ -1,6 +1,12 @@
 --1
---archive product_types on update or delete and create new
-
+--archive product_types on update or delete??? and create new???
+go
+create trigger archive on product 
+for update 
+as
+begin
+	
+end
 
 --2
 --on order update or insert change is_busy status of employees assigned employees
