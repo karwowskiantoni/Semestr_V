1f322f441485444a1743d55e87cd178cf9054540
kwisnia <230037@edu.p.lodz.pl>
2021-12-16 19:31:27 +0100

    setup

--- metaheurystyki/Zadanie_3/main.py
+++ metaheurystyki/Zadanie_3/main.py
@@ -1,10 +1,11 @@
 import random
+import numpy
 
 from Ant import Ant
 from Board import Board
 
 
-def print_best_distance(ants, board):
+def find_best_distance(ants, board):
     best_distance = ants[0].distance_traveled(board)
 
     for ant in ants:
@@ -13,25 +14,38 @@ def print_best_distance(ants, board):
     print(best_distance)
 
 
+def count_occurences(distances):
+    values, counts = numpy.unique(distances, return_counts=True)
+    return dict(zip(values, counts))
+
+
 if __name__ == '__main__':
-    POPULATION_SIZE = 100
-    RANDOM_FACTOR = 0.1
-    ALFA = 2
+    POPULATION_SIZE = 50
+    RANDOM_FACTOR = 0.3
+    ALFA = 1
     BETA = 1
-    ITERATION_NUMBER = 10
-    PHEROMONES_EVAPORATION_FACTOR = 0.2
+    ITERATION_NUMBER = 1000
+    PHEROMONES_EVAPORATION_FACTOR = 0.1
 
-    board = Board("data/P-n16-k8.txt")
+    board = Board(
+        "C:\\Users\\harry\\Downloads\\Semestr_V\\metaheurystyki\\Zadanie_3\\data\\A-n80-k10.txt")
 
-    board.print_distances()
-
-    for _ in range(ITERATION_NUMBER):
-        ants = [Ant(random.randint(1, len(board.places)) - 1) for _ in range(POPULATION_SIZE)]
+    # board.print_distances()
+    best_distances = []
+    for i in range(ITERATION_NUMBER):
+        ants = [Ant(random.randint(1, len(board.places)) - 1)
+                for _ in range(POPULATION_SIZE)]
 
         for _ in range(len(board.places) - 1):
             for ant in ants:
                 ant.next_step(board, ALFA, BETA, RANDOM_FACTOR)
         board.update_pheromones(PHEROMONES_EVAPORATION_FACTOR, ants)
-        print_best_distance(ants, board)
+        best_distances.append(
+            min([ant.distance_traveled(board) for ant in ants]))
+        print(min([ant.distance_traveled(board) for ant in ants]))
+    print()
+    print(min(best_distances))
+    print(numpy.average(best_distances))
+    print(numpy.median(best_distances))
 
-    board.print_pheromones()
+    # board.print_pheromones()
