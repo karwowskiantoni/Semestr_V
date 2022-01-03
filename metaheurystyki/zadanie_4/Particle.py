f34d0d7d9019762524d902ad3720ac3613c00b7d
antonikarwowski <229908@edu.p.lodz.pl>
2022-01-03 13:33:42 +0100

    initial commit metaheurystyki zadanie 4

---
+++ metaheurystyki/zadanie_4/Particle.py
@@ -0,0 +1,30 @@
+class Particle:
+    def __init__(self, x, y, inertia, cognitive_constant, social_constant, adaptation_function):
+        self.inertia = inertia
+        self.cognitive_constant = cognitive_constant
+        self.social_constant = social_constant
+        self.adaptation_function = adaptation_function
+
+        self.velocity = 0
+
+        self.x = x
+        self.y = y
+        self.actual_adaptation = 0
+
+        self.best_x = x
+        self.best_y = y
+        self.best_adaptation = 0
+
+    def calculate_adaptation(self):
+        return self.adaptation_function(self.x, self.y)
+
+    def update_velocity(self):
+        self.velocity = 0
+
+    def update_position(self):
+        self.x += self.velocity
+        self.y += self.velocity
+
+    def update_best_adaptation(self):
+        self.best_adaptation = max(self.actual_adaptation, self.calculate_adaptation())  # to źle ale chuj wiadomo o co chodzi
+
--- metaheurystyki/zadanie_4/main.py
+++ metaheurystyki/zadanie_4/main.py
@@ -1,2 +1,21 @@
+from tqdm import tqdm
+
+from Particle import Particle
+
 if __name__ == '__main__':
-    print("hello world")
+
+# no i użyj tu defa tak jak linter krzyczy
+    ACKLEY_FUNCTION = lambda x, y: x + y  # jebnij tu dwie funkcje z linka - "Ackley function" i "McCormick function"
+    MCCORMICK_FUNCTION = lambda x, y: x + y  # jebnij tu dwie funkcje z linka - "Ackley function" i "McCormick function"
+
+    ITERATION_NUMBER = 100
+    POPULATION_SIZE = 50
+    INERTIA = 1
+    COGNITIVE_CONSTANT = 1
+    SOCIAL_CONSTANT = 1
+    ADAPTATION_FUNCTION = MCCORMICK_FUNCTION
+
+    # particles = [Particle() for _ in range(POPULATION_SIZE)]
+
+    for _ in tqdm(range(ITERATION_NUMBER)):
+        print("gowno")
