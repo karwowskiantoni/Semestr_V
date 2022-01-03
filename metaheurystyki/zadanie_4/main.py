9b42929ab50bdb8e6489d6de29f3d97081276f2e
Serionus <68148246+Serionus@users.noreply.github.com>
2022-01-03 13:50:27 +0100

    functions

--- metaheurystyki/zadanie_4/main.py
+++ metaheurystyki/zadanie_4/main.py
@@ -1,19 +1,24 @@
+from numpy import pi, e, sin, exp, sqrt, cos
 from tqdm import tqdm
-
 from Particle import Particle
 
 if __name__ == '__main__':
 
-# no i użyj tu defa tak jak linter krzyczy
-    ACKLEY_FUNCTION = lambda x, y: x + y  # jebnij tu dwie funkcje z linka - "Ackley function" i "McCormick function"
-    MCCORMICK_FUNCTION = lambda x, y: x + y  # jebnij tu dwie funkcje z linka - "Ackley function" i "McCormick function"
+    def ackley_function(x, y):
+        return -20.0 * exp(-0.2 * sqrt(0.5 * (pow(x, 2) + pow(y, 2)))) - exp(0.5 * (cos(2 * pi * x) + cos(
+            2 * pi * y))) + e + 20
+
+
+    def mccormic_function(x, y):
+        return sin(x + y) + pow(x - y, 2) - 1.5 * x + 2.5 * y + 1
+
 
     ITERATION_NUMBER = 100
     POPULATION_SIZE = 50
     INERTIA = 1
     COGNITIVE_CONSTANT = 1
     SOCIAL_CONSTANT = 1
-    ADAPTATION_FUNCTION = MCCORMICK_FUNCTION
+    ADAPTATION_FUNCTION = ackley_function()
 
     # particles = [Particle() for _ in range(POPULATION_SIZE)]
 
